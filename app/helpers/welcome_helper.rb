9b31c03d62f264cdb17e7b09a52dae5a42dd3a5a
Nicholas Schlueter <schlueter@gmail.com>
2009-01-31 00:18:21 -0500

    added tracking and contact info

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,14 +1,14 @@
 module WelcomeHelper
-  def twitter_image
-    link_to image_tag("http://assets1.twitter.com/images/favicon.ico"), "http://twitter.com"
+  def twitter_image(user = false)
+    link_to image_tag("http://assets1.twitter.com/images/favicon.ico"), "http://twitter.com/#{user ? 'schlu' : ''}"
   end
   
-  def flickr_image
-    link_to image_tag("http://flickr.com/favicon.ico"), "http://flickr.com"
+  def flickr_image(user = false)
+    link_to image_tag("http://flickr.com/favicon.ico"), "http://flickr.com/#{user ? 'dcrails' : ''}"
   end
   
-  def momentile_image
-    link_to image_tag("http://momentile.com/favicon.ico"), "http://momentile.com"
+  def momentile_image(user = false)
+    link_to image_tag("http://momentile.com/favicon.ico"), "http://momentile.com/#{user ? 'schlu' : ''}"
   end
   
   def simpltry_image
